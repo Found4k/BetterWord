2861939b9fecc982f824682618bbd5d22f0322a8
Found4k <88767692+Found4k@users.noreply.github.com>
2021-08-25 14:45:23 +1000

    Add files via upload

---
+++ BetterWord.py
@@ -0,0 +1,7 @@
+print('BETTERWORD')
+print('BetterWord is currently in beta and is not finished! Type Credts to see credits.')
+credits = input('>>> ')
+if credits == "Credits":
+    print('Programmed by Found4k in python')
+    print('Help from Jensen .B')
+    
---
+++ BwordCreateNew.py
@@ -0,0 +1,4 @@
+print('BetterWord 0.0.1 2021 edition')
+while True:
+    text = input('')
+    print(text)
